f25c6914d70d4b9fe45bbabb678527d7714d1aac
heymeepo <h3ystudio@gmail.com>
2024-02-22 09:24:22 +0400

    Add BatchMaterialPropertyAttribute

---
+++ Runtime/BatchMaterialPropertyAttribute/BatchMaterialPropertyAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Scellecs.Morpeh.Graphics
+{
+    [AttributeUsage(AttributeTargets.Struct, AllowMultiple = false)]
+    public sealed class BatchMaterialPropertyAttribute : Attribute
+    {
+        public string MaterialPropertyId { get; } 
+        public BatchMaterialPropertyFormat Format { get; }
+
+        public BatchMaterialPropertyAttribute(string materialPropertyId, BatchMaterialPropertyFormat format)
+        {
+            MaterialPropertyId = materialPropertyId;
+            Format = format;
+        }
+    }
+}
